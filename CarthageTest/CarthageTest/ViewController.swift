4efbea5b322c80ac52a532cbc187b0d2e9ded77c
Devon <devon@alumni.ubc.ca>
2015-01-04 23:19:17 -0800

    Initial commit

---
+++ CarthageTest/CarthageTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarthageTest
+//
+//  Created by Devon on 2015-01-04.
+//  Copyright (c) 2015 Devon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CarthageTest/CarthageTestTests/CarthageTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  CarthageTestTests.swift
+//  CarthageTestTests
+//
+//  Created by Devon on 2015-01-04.
+//  Copyright (c) 2015 Devon. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CarthageTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
